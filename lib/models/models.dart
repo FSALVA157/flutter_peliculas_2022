9b783f969f06fa7a9c45b5a1091b50df708084f3
xxavierargentino <xxavierargentino@hotmail.com>
2022-05-12 19:36:38 -0300

    modelado de nowPlaying respons de Moviedb

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,2 +1,6 @@
 export 'package:fl_peliculas/models/routes_model.dart';
+export 'package:fl_peliculas/models/now_playing_response.dart';
+export 'package:fl_peliculas/models/movie.dart';
+
+
 
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,15 +1,28 @@
+import 'dart:convert';
 
+import 'package:fl_peliculas/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier{
+  final String _base_url = 'api.themoviedb.org';
 
+  
   MoviesProvider(){
     print('movie provider llamado');
-    this.getMovies();
+    this.getOnDisplayMovies();
   }
 
-  getMovies(){
-    print('getMovies llamado');
+  getOnDisplayMovies() async{
+    var url = Uri.https(_base_url, '3/movie/now_playing', {
+  'api_key': '6d6d52189580fcb80212d917146ea0fa',
+  'language': 'es-ES',
+  'page': '1'
+  });
+  var response = await http.get(url);
+  final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+  //final Map<String, dynamic> data = json.decode(response.body);
+    print(nowPlayingResponse.results);
   }
 
 }
